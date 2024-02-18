e03dc7369aede93c6f8fc06512c2688f567da60e
procrastinator <ihrishikeshnate@gmail.com>
2024-02-18 21:16:25 +0530

    README UPDATED

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+cart = []
+
+@app.route("/health",methods=['GET'])
+def health():
+    return "2001"
+
+@app.route("/add",methods=['GET','POST'])
+def add_to_cart():
+    id = request.args.get('id')
+    product_id = request.args.get('product')
+    print(id,product_id)
+    return "<h1>200</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True,port=3000)
