646b28b3752591283dbc4461975f37d5e15227ab
kolambkar11 <kolambkarashutosh@gmail.com>
2024-03-02 00:48:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './components/navigation/Navigation';
+import Products from './components/products/Products';
+function App() {
+  return (
+    <>
+    <Navigation/>
+
+    <Products/>
+      
+    </>
+  );
+}
+
+export default App;
